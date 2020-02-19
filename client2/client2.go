b266bc6cd556900653ce35d936137c4f65300266
dkomarovo <dkomarov@hawk.iit.edu>
2020-02-19 00:11:53 -0600

    add detection for local certs and generated new ones
    still in the process of trying to get the system to trust self-signed certs. so that post requests can come thru and that data can be delivered successfully

--- client2/client2.go
+++ client2/client2.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-
+	"io/ioutil"
 	"fmt"
 	"log"
 	"net/http"
-  "net/http/httputil"
-	"net/url"
+  //"net/http/httputil"
+	//"net/url"
 //  "crypto/tls"
-//  "crypto/x509"
-//  "io/ioutil"
-	"time"
-	
+ // "crypto/x509"
+ // "io/ioutil"
+	//"time"
+
 	//"github.com/warthog618/gpio"
-	"os"
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/mp3"
-	"github.com/faiface/beep/speaker"
+	//"os"
+	//"github.com/faiface/beep"
+	//"github.com/faiface/beep/mp3"
+	//"github.com/faiface/beep/speaker"
 
 	"github.com/gorilla/mux"
+	"crypto/x509"
+	"crypto/tls"
+	"crypto/rand"
 
 )
 
@@ -39,34 +42,99 @@ func main() {
 
 	//go listenForSmoke()
 	log.Output(1, "Listening for smoke")
-/*
-  caCert, err := ioutil.ReadFile("../server/server.crt")
-       if err != nil {
-               log.Fatal(err)
-       }
-       caCertPool := x509.NewCertPool()
-       caCertPool.AppendCertsFromPEM(caCert)
-
-   client := &http.Client{
-                Transport: &http.Transport{
-                       TLSClientConfig: &tls.Config{
-                               RootCAs: caCertPool,
-                      },
-                },
-}*/
 
 	// start the http server to listen to requests from the server
 	router := mux.NewRouter()
 	router.HandleFunc("/lights", handleRequests)
-	//log.Fatal(http.ListenAndServe(":12345", router))
+  //log.Fatal(http.ListenAndServe(":12345", router))
 
-  localProxyUrl, _ := url.Parse("http://127.0.0.1:8200/")
-	localProxy := httputil.NewSingleHostReverseProxy(localProxyUrl)
-	http.Handle("/", localProxy)
+  // localProxyUrl, _ := url.Parse("http://127.0.0.1:8100/")
+	// localProxy := httputil.NewSingleHostReverseProxy(localProxyUrl)
+	// http.Handle("/", localProxy)
 
-	log.Println("Serving on localhost:12345")
-	log.Fatal(http.ListenAndServeTLS(":12345", "server.crt", "server.key", router))
+	server := &http.Server{
+			Addr:         ":12345",
+			Handler:      router,
+		}
+
+	server.TLSConfig = configTLS()
+
+	log.Println("About to listen on 12345. Go to https://localhost:12345")
+	log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
+	//log.Fatal(http.ListenAndServeTLS(":12345", "cert.pem", "key.pem", router))
+
+
 }
+
+func configTLS() *tls.Config {
+	TLSConfig := &tls.Config{}
+	// TLSConfig.CipherSuites = []uint16{
+	// 	tls.TLS_FALLBACK_SCSV,
+	// 	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	// 	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+	// 	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+	// 	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	// 	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	// 	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+	// 	tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+	// 	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+	// 	tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+	// 	tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+	// }
+
+	TLSConfig.Rand = rand.Reader
+	TLSConfig.MinVersion = tls.VersionTLS10
+	TLSConfig.SessionTicketsDisabled = false
+	TLSConfig.InsecureSkipVerify = false
+	TLSConfig.ClientAuth = tls.VerifyClientCertIfGiven
+	TLSConfig.PreferServerCipherSuites = true
+	TLSConfig.ClientSessionCache = tls.NewLRUClientSessionCache(1000)
+	TLSConfig.RootCAs = loadCertificates()
+
+	return TLSConfig
+}
+
+
+func loadCertificates() *x509.CertPool {
+
+	// rootPEM := `-----BEGIN CERTIFICATE-----
+	// MIIDQDCCAigCCQDKGKeSZN7AbTANBgkqhkiG9w0BAQsFADBiMQswCQYDVQQGEwJV
+	// UzELMAkGA1UECAwCSUwxEDAOBgNVBAcMB0NoaWNhZ28xDDAKBgNVBAoMA1BPRTEM
+	// MAoGA1UECwwDNTEwMRgwFgYDVQQDDA9sb2NhbGhvc3Q6MTIzNDUwHhcNMjAwMjE5
+	// MDE0NjI2WhcNMjEwMjE4MDE0NjI2WjBiMQswCQYDVQQGEwJVUzELMAkGA1UECAwC
+	// SUwxEDAOBgNVBAcMB0NoaWNhZ28xDDAKBgNVBAoMA1BPRTEMMAoGA1UECwwDNTEw
+	// MRgwFgYDVQQDDA9sb2NhbGhvc3Q6MTIzNDUwggEiMA0GCSqGSIb3DQEBAQUAA4IB
+	// DwAwggEKAoIBAQCXiUJkn8dr0KPPUStDSn3ym0atEB9yqvqAxHYewkVAwvNBD9F7
+	// a3uYfWR6oAUIOJkfLwdW5N8v/+MKH15jMmbLNyCM3whcwg7upXY/aBMSw4PWcMAY
+	// RjhAn788P9MEjcHJY02TSx/Bnaz2PXwtPcR/d11fpeS0OyPK+fO2AMxr7wFIjipY
+	// mufV49QdU6RK1Yie9wcd5//aCW3GFJVRWW1HfTR65WjdAmIH1MtmmRyH8CVD6wTb
+	// eZjpeNElaObT8afAGjAS0AUdk7qqd+wrN+6hxhwxo8/OjsMVu1woGh1/hmkY057H
+	// +m5doAWSl641Ed+8hYn6ZY07ep77CdOXPA7bAgMBAAEwDQYJKoZIhvcNAQELBQAD
+	// ggEBAFUDBzEZ6LYtDd/LKXDvn8dvmamnX/ZpXGRWXRLdXqGaLHelbrpFxIq64b8s
+	// YtuYxP8aGLZ7AB7efnDFNq5UjP0ce+R/8xyl2UjQKnpKcNNMUctOrzKv6wWafrun
+	// qAeHAutKZFuI1t1r3Hf/vkHE4hjDKlMvGWVkQXU6KVgtAAx1OUAZDiR278wJHQlU
+	// j+fPfYyazl+kWCEzrR1e4NSNUdDkQPT6SQy0L1MxjnySGu0yuCcHqwsPEt4lBCX0
+	// 9j0Fe2aq2QhaqDiQd2eDo/gMCWE++D/LGvJaYRL0N8L8CtcvXQTfK04cKsr201Nk
+	// luVD4BNNQ5dj0rGtXDBkaW0WERI=
+	// -----END CERTIFICATE-----`
+
+	pem, err := ioutil.ReadFile("cert.pem")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// rootCAs := x509.NewCertPool()
+	// rootCAs.AppendCertsFromPEM([]byte(rootPEM))s
+
+	rootCertPool := x509.NewCertPool()
+	if !rootCertPool.AppendCertsFromPEM(pem) {
+		panic("Failed appending certs")
+	}
+
+	return rootCertPool
+}
+
 /*
 func initPins() (err error) {
 	err = gpio.Open()
@@ -86,9 +154,9 @@ func initPins() (err error) {
 	envOutPin = gpio.NewPin(24)
 	envOutPin.SetMode(gpio.Output)
 	smokePin = gpio.NewPin(13)
-	
+
 	log.Output(1, "Pins initialized")
-	
+
 	return err
 
 }
@@ -97,7 +165,7 @@ func initPins() (err error) {
 /*
 func writeToGPIO(emergencyType string) {
 	log.Output(1, "Writing to GPIO")
-	
+
 	switch emergencyType {
 	case "Fire":
 		triggerButton(fireOutPin)
@@ -110,7 +178,7 @@ func writeToGPIO(emergencyType string) {
 
 	case "Enviormental":
 		triggerButton(envOutPin)
-		audio("./audio/env.mp3")	
+		audio("./audio/env.mp3")
 	}
 }
 */
@@ -128,7 +196,7 @@ func listenForSmoke() {
 		time.Sleep(1 *time.Millisecond)
 	}
 
-	
+
 
 }
 */
@@ -138,7 +206,7 @@ func triggerButton(pin *gpio.Pin) {
 	pin.Write(gpio.Low)
 	time.Sleep(250 * time.Millisecond)
 	pin.Write(gpio.High)
-	
+
 
 }*/
 
@@ -148,19 +216,19 @@ func triggerButton(pin *gpio.Pin) {
 func handleRequests(w http.ResponseWriter, r *http.Request) {
 	log.Output(1, "handling request from webpage")
 
- 
+
     switch r.Method {
 	case "POST":
 		log.Output(1, "Post Request Recieved")
 
         // Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
 		err := r.ParseForm()
-		
+
 		if err != nil {
 			log.Output(1, err.Error())
 		}
 
-	
+
 		// parses emergency type from post request
 		emergencyType := r.Form["emergency"][0]
 		fmt.Println(emergencyType)
@@ -180,14 +248,14 @@ func handleRequests(w http.ResponseWriter, r *http.Request) {
 		}
 */
 
-        
+
     default:
         fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
     }
 }
 
 
-
+/*
 func audio(pathToFile string) error {
 	log.Output(1, "Playing Audio")
 	f, err := os.Open(pathToFile)
@@ -213,4 +281,4 @@ func audio(pathToFile string) error {
 
 	return err
 
-}
+} */
--- server/server.go
+++ server/server.go
@@ -1,18 +1,29 @@
 package main
 
 import (
-	
+	"io/ioutil"
 	"log"
 	"fmt"
 	"net/http"
-	"net/http/httputil"
+	//"net/http/httputil"
 	"github.com/GeertJohan/go.rice"
 	"github.com/gorilla/mux"
 	"net/url"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/rand"
+	//"math/rand"
+	//"time"
 )
 
 
 func main() {
+
+	 // ok := roots.AppendCertsFromPEM([]byte(rootPEM))
+	 // if !ok {
+		// 	 panic("failed to parse root certificate")
+	 // }
+
 	startWebApp()
 
 }
@@ -20,44 +31,123 @@ func main() {
 
 func startWebApp() {
 	router := mux.NewRouter()
+	//router.HandleFunc("/button", handleRequests)
 	router.HandleFunc("/button", handleRequests)
 	router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("../website").HTTPBox())) // starts the web UI
+  //router.Schemes("https")
 	log.Output(1, "Started Web UI and http server")
+
+	server := &http.Server{
+			Addr:         ":8081",
+			Handler:      router,
+		}
+
+	server.TLSConfig = configTLS()
+
 	//log.Fatal(http.ListenAndServe(":8080", router))
 
-	localProxyUrl, _ := url.Parse("http://127.0.0.1:8100/")
-	localProxy := httputil.NewSingleHostReverseProxy(localProxyUrl)
-	http.Handle("/", localProxy)
+	// localProxyUrl, _ := url.Parse("http://127.0.0.1:8100/")
+	// localProxy := httputil.NewSingleHostReverseProxy(localProxyUrl)
+	// http.Handle("/", localProxy)
 
-	log.Println("Serving on localhost:8080")
-	log.Fatal(http.ListenAndServeTLS(":8080", "server.crt", "server.key", router))
+	log.Println("About to listen on 8081. Go to https://localhost:8081")
+	log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
+	//log.Fatal(http.ListenAndServeTLS(":8081", "cert.pem", "key.pem", router))
+
+}
+
+func configTLS() *tls.Config {
+	TLSConfig := &tls.Config{}
+	// TLSConfig.CipherSuites = []uint16{
+	// 	tls.TLS_FALLBACK_SCSV,
+	// 	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	// 	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+	// 	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+	// 	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	// 	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	// 	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+	// 	tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+	// 	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+	// 	tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+	// 	tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+	// }
+
+	TLSConfig.Rand = rand.Reader
+	TLSConfig.MinVersion = tls.VersionTLS10
+	TLSConfig.SessionTicketsDisabled = false
+	TLSConfig.InsecureSkipVerify = false
+	TLSConfig.ClientAuth = tls.VerifyClientCertIfGiven
+	TLSConfig.PreferServerCipherSuites = true
+	TLSConfig.ClientSessionCache = tls.NewLRUClientSessionCache(1000)
+	TLSConfig.RootCAs = loadCertificates()
+
+	return TLSConfig
+}
+
+func loadCertificates() *x509.CertPool {
+
+	// rootPEM := `-----BEGIN CERTIFICATE-----
+	// MIIDPjCCAiYCCQC2wBvwPhDsVjANBgkqhkiG9w0BAQsFADBhMQswCQYDVQQGEwJV
+	// UzELMAkGA1UECAwCSUwxEDAOBgNVBAcMB0NoaWNhZ28xDDAKBgNVBAoMA1BPRTEM
+	// MAoGA1UECwwDNTEwMRcwFQYDVQQDDA5sb2NhbGhvc3Q6ODA4MTAeFw0yMDAyMTkw
+	// MTQ1NThaFw0yMTAyMTgwMTQ1NThaMGExCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJJ
+	// TDEQMA4GA1UEBwwHQ2hpY2FnbzEMMAoGA1UECgwDUE9FMQwwCgYDVQQLDAM1MTAx
+	// FzAVBgNVBAMMDmxvY2FsaG9zdDo4MDgxMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A
+	// MIIBCgKCAQEAsm3vDUSFqSXcdzT+2YT1ZwLoQ/HyOMsg1e/OfPp0tvDpoqhG4+Wo
+	// KrcAWwh3/XVBg/NwMwy9mqkinPeRsSaFMrlWMf1SRSxNKkx1HywCMzv5DTQJrU8L
+	// Hdqh9MaaA4sAEKTBKeeexvtQT37dScuwqMo0We73mHegv+ux9ByNSIW8yA1VbjtC
+	// 9Cyf8eWVLibFZA6zIS6Qf+yEpr3qZ0Ukt3fg1FZFT64sJ1XVJvG3CgMq+P9Rb+Zc
+	// XWBlc0NQpt0H/Ok2TATQXPbPyXAUN3JJnhtmjNowiSJXsxAo6I+mNB0eyLeDSATu
+	// s5weUubGXlO+P7Ey06dl2gXGQIx1JULXUQIDAQABMA0GCSqGSIb3DQEBCwUAA4IB
+	// AQCCOE8znV66/LpmNsZm9gPM1PeVN4GkFFn6uZMhJTatwlTuO95FV7SCtP2vJTMc
+	// +8Fe+Mp1QlXxCxPVUjVzZZwQkPKeV83Ht09kFloQNFD+afb9c8Q1IxC6YtBgt4VG
+	// M+CfGCTl208qwOL7TiP5hAzUCiAh+tMYnzcl16q8BtcBrhDeO/LfSQDBkRgHVr1/
+	// zJJ8E1Cn5uE4UTN43mUpxUE7VdC3KrKC9lcx/H5YrYer2E/gRb0Rk8oYM7KShqPm
+	// 5f6KJFgcZNPGsXuQw31YSsi1dt/QdnZg7u9dDS4TkHzils/7zlNtCWwvVrWZ9TU/
+	// vKmXKUCKTAEIzwlS0LrqMNrs
+	// -----END CERTIFICATE-----`
+
+	pem, err := ioutil.ReadFile("cert.pem")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// rootCAs := x509.NewCertPool()
+	// rootCAs.AppendCertsFromPEM([]byte(rootPEM))s
+
+	rootCertPool := x509.NewCertPool()
+	if !rootCertPool.AppendCertsFromPEM(pem) {
+		panic("Failed appending certs")
+	}
+
+	return rootCertPool
 }
 
 
 func handleRequests(w http.ResponseWriter, r *http.Request) {
 	log.Output(1, "handling request from webpage")
 
- 
+
     switch r.Method {
 	case "POST":
-		
+
 		log.Output(1, "Post Request Recieved")
 		err := r.ParseForm()
-		
+
 		if err != nil {
 			log.Output(1, err.Error())
 		}
 
-		
+
 		emergencyType := r.Form["emergency"][0] // get the emergency out of the POST request
 		err = sendMessage(emergencyType)  // send the message to the ceiling client
-
 		if err != nil {
 			log.Output(1, err.Error())
 
 		}
 
-        
+
     default:
         fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
     }
@@ -66,7 +156,7 @@ func handleRequests(w http.ResponseWriter, r *http.Request) {
 
 
 func sendMessage(emergencyType string) (err error) {
-
+  fmt.Println("Attempting to send message...")
 	APIURL := "https://localhost:12345/lights"
 
 	response, err := http.PostForm(APIURL,
@@ -76,7 +166,7 @@ func sendMessage(emergencyType string) (err error) {
 		return err
 	}
 
-	fmt.Println(response)
+	fmt.Println("response is: " , response)
 
 	return err
 }
